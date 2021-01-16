38ee5f567d05ce5454c96657d7f2a7c2c71d1d9e
Henry Winget <henrywinget@gmail.com>
2021-01-15 22:58:04 -0600

    testing travis ci

--- src/App.js
+++ src/App.js
@@ -11,44 +11,45 @@ class App extends Component {
   state = {
     characters: []
   };
-  
+
   componentDidMount() {
+    console.log('getting all characters');
     this.getAllCharacters();
   }
-  
+
   // gets all of our DnD characters (nice)
   getAllCharacters = () => {
     API.doGet("/all_characters", data => {
-      if(data.length > 0) {
+      if (data.length > 0) {
         this.setState({ characters: data })
       }
     })
   };
-  
+
   postNewCharacter = (character, cb) => {
     API.doPost("/create_character", character, data => {
-      if(data) {
+      if (data) {
         cb();
         this.getAllCharacters();
       }
     })
   };
-  
+
   deleteCharacter = id => {
-    if(window.confirm('Are you sure you want to delete this character? This is a permanent action!')) {
+    if (window.confirm('Are you sure you want to delete this character? This is a permanent action!')) {
       API.doDelete("/delete_character/" + id, null, () => {
         this.getAllCharacters();
       })
     }
   };
-  
+
   render() {
     return (
       <div className="App">
-        <NavBar/>
+        <NavBar />
         <Party postNewCharacter={this.postNewCharacter}
-               deleteCharacter={this.deleteCharacter}
-               characters={this.state.characters}/>
+          deleteCharacter={this.deleteCharacter}
+          characters={this.state.characters} />
       </div>
     );
   }
